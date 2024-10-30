7298334e00d1b2f5ae84a044012c8d23f04cbb7a
Oliver Foggin <oliverfoggin@me.com>
2024-10-30 12:55:09 +0000

    Update BundleCurrentPlugin.swift

--- Plugins/BundleCurrent/BundleCurrentPlugin.swift
+++ Plugins/BundleCurrent/BundleCurrentPlugin.swift
@@ -70,11 +70,11 @@ func generateBundleExtensionSwift(packageName: String, targetName: String) -> St
             /* Bundle should be present here when the package is linked into an App. */
             Bundle.main.resourceURL,
             /* Bundle should be present here when the package is linked into a framework. */
-            Bundle(for: CurrentBundleFinder.self).resourceURL,
+            Bundle(for: BundleFinder.self).resourceURL,
             /* For command-line tools. */
             Bundle.main.bundleURL,
             /* Bundle should be present here when running previews from a different package (this is the path to "…/Debug-iphonesimulator/"). */
-            Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
+            Bundle(for: BundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
         ]
         for candidate in candidates {
             let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
